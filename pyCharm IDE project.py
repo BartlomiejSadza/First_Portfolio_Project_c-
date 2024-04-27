952aa8573c4dbff2b2a4b17dc16bda5753a7ffb5
Bartlomiej Sadza <bartlomiejsadza@icloud.com>
2024-04-27 20:48:13 +0200

    Made a function for avgAge

--- pyCharm IDE project.py
+++ pyCharm IDE project.py
@@ -3,19 +3,31 @@ import csv
 with open('insurance.csv', 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
 
-    list_of_age = []
     counter_of_rows = 0
     for row in csv_reader:
         counter_of_rows += 1
-        list_of_age.append(row[0])
     print("In our database there are " + str(counter_of_rows) + " patients who have insurance.")
+    csvfile.seek(0)
 
-    list_of_age.pop(0)
+    csv_reader = csv.reader(csvfile)
 
     # Counting an average age of patient  c;
-    counter_of_age = 0
-    for age in list_of_age:
-        counter_of_age += int(age)
-    average_full = counter_of_age / len(list_of_age)
-    average = round(average_full, 2)
-    print('\nThe average age of insurance patient is ' + str(average) + ' years old.')
+    def average_age(csvreader):
+
+        list_of_age = []
+        for row in csvreader:
+            list_of_age.append(row[0])
+        list_of_age.pop(0)
+        print(list_of_age)
+
+        counter_of_age = 0
+        for age in list_of_age:
+            counter_of_age += int(age)
+
+        average_full = counter_of_age / len(list_of_age)
+        average = round(average_full, 2)
+
+
+        print('\nThe average age of insurance patient is ' + str(average) + ' years old.')
+
+    average_age(csv_reader)
