f9492b96c6c9fd38653dceb6a6c2e13f95ef7922
Bartlomiej Sadza <bartlomiejsadza@icloud.com>
2024-05-09 23:11:24 +0200

    pyCharm\ IDE\ project.py

--- pyCharm IDE project.py
+++ pyCharm IDE project.py
@@ -9,6 +9,12 @@ with open('insurance.csv', 'r') as csvfile:
     print("In our database there are " + str(counter_of_rows) + " patients who have insurance.")
     csvfile.seek(0)
 
+    print('\n')
+
+    for row in csv_reader:
+        print(row)
+        break
+
     csv_reader = csv.reader(csvfile)
 
     # Counting an average age of patient  c;
@@ -18,7 +24,6 @@ with open('insurance.csv', 'r') as csvfile:
         for row in csvreader:
             list_of_age.append(row[0])
         list_of_age.pop(0)
-        print(list_of_age)
 
         counter_of_age = 0
         for age in list_of_age:
@@ -31,3 +36,15 @@ with open('insurance.csv', 'r') as csvfile:
         print('\nThe average age of insurance patient is ' + str(average) + ' years old.')
 
     average_age(csv_reader)
+    csvfile.seek(0)
+
+    print()
+
+    def average_costs(csvreader):
+        list_of_costs = []
+        for row in csvreader:
+            list_of_costs.append(row[0])
+
+        print(list_of_costs)
+
+    average_costs(csv_reader)
