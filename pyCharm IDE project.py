dd8f5dac0b9196008632edbaa93eb031ffcbdf8c
Bartlomiej Sadza <bartlomiejsadza@icloud.com>
2024-05-09 23:12:56 +0200

    Calculated average cost per person (:

--- pyCharm IDE project.py
+++ pyCharm IDE project.py
@@ -3,17 +3,16 @@ import csv
 with open('insurance.csv', 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
 
+    for row in csv_reader:
+        print(row)
+        break
+    print('\n')
+
     counter_of_rows = 0
     for row in csv_reader:
         counter_of_rows += 1
     print("In our database there are " + str(counter_of_rows) + " patients who have insurance.")
     csvfile.seek(0)
-
-    print('\n')
-
-    for row in csv_reader:
-        print(row)
-        break
 
     csv_reader = csv.reader(csvfile)
 
@@ -38,13 +37,18 @@ with open('insurance.csv', 'r') as csvfile:
     average_age(csv_reader)
     csvfile.seek(0)
 
-    print()
-
+    # Counting an average costs for individual (:
     def average_costs(csvreader):
         list_of_costs = []
         for row in csvreader:
-            list_of_costs.append(row[0])
+            list_of_costs.append(row[-1])
+        list_of_costs.pop(0)
 
-        print(list_of_costs)
+        count_cost = 0
+        for cost in list_of_costs:
+            count_cost += float(cost)
+
+        avg_cost = count_cost / len(list_of_costs)
+        print('The average cost for person carry out: ' + str(round(avg_cost, 2)) + ' dollars.')
 
     average_costs(csv_reader)
